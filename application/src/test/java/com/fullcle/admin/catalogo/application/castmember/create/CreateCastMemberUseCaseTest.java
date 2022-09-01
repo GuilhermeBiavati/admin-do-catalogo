76245b07916565ddc07047af7446dc804c120cdc
Guilherme Antonio Biavati Cagnini <guilherme.cagnini@ciss.com.br>
2022-09-01 13:34:32 -0300

    Teste create castmember usecase

--- application/src/test/java/com/fullcle/admin/catalogo/application/castmember/create/CreateCastMemberUseCaseTest.java
+++ application/src/test/java/com/fullcle/admin/catalogo/application/castmember/create/CreateCastMemberUseCaseTest.java
@@ -18,9 +18,9 @@ import org.mockito.Mockito;
 
 import com.fullcle.admin.catalogo.application.Fixture;
 import com.fullcle.admin.catalogo.application.UseCaseTest;
-import com.fullcle.admin.catalogo.application.castmember.create.CreateCastMemberCommand;
-import com.fullcle.admin.catalogo.application.castmember.create.DefaultCreateCastMemberUseCase;
 import com.fullcle.admin.catalogo.domain.castmember.CastMemberGateway;
+import com.fullcle.admin.catalogo.domain.castmember.CastMemberType;
+import com.fullcle.admin.catalogo.domain.exceptions.NotificationException;
 
 public class CreateCastMemberUseCaseTest extends UseCaseTest {
 
@@ -78,77 +78,68 @@ public class CreateCastMemberUseCaseTest extends UseCaseTest {
     public void givenAInvalidName_whenCallsCreateCastMember_thenShouldReturnDomainException() {
 
         final String expectedName = null;
-        final var expectedDescription = "A categoria mais assistida";
-        final var expectedIsActive = true;
+        final var expectedType = Fixture.CastMember.type();
         final var expectedErrorMessage = "'name' should not be null";
         final var expectedErrorCount = 1;
 
-        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedDescription, expectedIsActive);
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
 
-        final var notification = useCase.execute(aCommand).getLeft();
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> useCase.execute(aCommand));
 
-        Assertions.assertEquals(expectedErrorCount, notification.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
 
         Mockito.verify(castMemberGateway, times(0)).create(any());
 
     }
 
     @Test
-    public void givenAValidCommandWithInactiveCastMember_whenCallsCreateCastMember_shouldReturnInactiveCastMemberId() {
+    public void givenAInvalidType_whenCallsCreateCastMember_thenShouldReturnDomainException() {
 
-        final var expectedName = "Filmes";
-        final var expectedDescription = "A categoria mais assistida";
-        final var expectedIsActive = false;
+        final String expectedName = Fixture.name();
+        final CastMemberType expectedType = null;
+        final var expectedErrorMessage = "'type' should not be null";
+        final var expectedErrorCount = 1;
 
-        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedDescription, expectedIsActive);
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
 
-        when(castMemberGateway.create(any())).thenAnswer(returnsFirstArg());
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> useCase.execute(aCommand));
 
-        final var actualOutput = useCase.execute(aCommand).get();
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
 
-        Assertions.assertNotNull(actualOutput);
-        Assertions.assertNotNull(actualOutput.id());
+        Mockito.verify(castMemberGateway, times(0)).create(any());
 
-        Mockito.verify(castMemberGateway, times(1))
-                .create(argThat(aCastMember -> Objects.equals(expectedName, aCastMember.getName())
-                        && Objects.equals(expectedDescription, aCastMember.getDescription())
-                        && Objects.equals(expectedIsActive, aCastMember.isActive())
-                        && Objects.nonNull(aCastMember.getId())
-                        && Objects.nonNull(aCastMember.getCreatedAt())
-                        && Objects.nonNull(aCastMember.getUpdatedAt())
-                        && Objects.nonNull(aCastMember.getUpdatedAt())
-                        && Objects.nonNull(aCastMember.getDeletedAt())
-                ));
     }
 
     @Test
     public void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnAException() {
 
         final var expectedName = "Filmes";
-        final var expectedDescription = "A categoria mais assistida";
-        final var expectedIsActive = true;
+        final var expectedType = Fixture.CastMember.type();
         final var expectedErrorMessage = "Gateway error";
         final var expectedErrorCount = 1;
 
-        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedDescription, expectedIsActive);
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
 
         when(castMemberGateway.create(any())).thenThrow(new IllegalStateException("Gateway error"));
 
-        final var notification = useCase.execute(aCommand).getLeft();
+        final var actualOutput = Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(aCommand));
 
-        Assertions.assertEquals(expectedErrorCount, notification.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessage, notification.firstError().message());
+//        Assertions.assertEquals(expectedErrorCount, actualOutput.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualOutput.getMessage());
 
         Mockito.verify(castMemberGateway, times(1))
-                .create(argThat(aCastMember -> Objects.equals(expectedName, aCastMember.getName())
-                        && Objects.equals(expectedDescription, aCastMember.getDescription())
-                        && Objects.equals(expectedIsActive, aCastMember.isActive())
-                        && Objects.nonNull(aCastMember.getId())
-                        && Objects.nonNull(aCastMember.getCreatedAt())
-                        && Objects.nonNull(aCastMember.getUpdatedAt())
-                        && Objects.nonNull(aCastMember.getUpdatedAt())
-                        && Objects.isNull(aCastMember.getDeletedAt())
+                .create(argThat(aCastMember ->
+                        Objects.equals(expectedName, aCastMember.getName())
+                                && Objects.equals(expectedType, aCastMember.getType())
+                                && Objects.nonNull(aCastMember.getId())
+                                && Objects.nonNull(aCastMember.getCreatedAt())
+                                && Objects.nonNull(aCastMember.getUpdatedAt())
                 ));
     }
 }
